9ea5ed013bb84bf66a0dc83e9ceda3fd652d1597
Hajun Park <jbaltop@gmail.com>
2019-06-21 16:40:00 +0900

    refactor

--- system_info.py
+++ system_info.py
@@ -25,19 +25,18 @@ import wmi
 
 PHYSICAL_DISK_TAG = "\\\\.\\PHYSICALDRIVE0"
 
+FILE = Path(__file__)
+PROJECT = FILE.parent
+OUTPUT = PROJECT / "output"
+LOG = OUTPUT / "log"
+LOG_FILE = LOG / f"{FILE.stem}.log"
+
 # create log directory
-file = Path(__file__)
-output_path = file.parent / "output"
-log_path = output_path / "log"
-
-if not output_path.exists():
-    Path.mkdir(output_path)
-
-if not log_path.exists():
-    Path.mkdir(log_path)
+if not Path.exists(LOG):
+    Path.mkdir(LOG, parents=True)
 
 # create logger, set logging level
-logger = logging.getLogger('system_info')
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 # set time zone to utc
@@ -45,7 +44,7 @@ logging.Formatter.converter = time.gmtime
 
 # create file handler
 fh = RotatingFileHandler(
-    'output/log/system_info.log',
+    LOG_FILE,
     'at',
     encoding='utf-8',
     maxBytes=50*1024*1024,
@@ -55,16 +54,19 @@ fh.setLevel(logging.DEBUG)
 
 # create console handler
 ch = logging.StreamHandler()
-ch.setLevel(logging.WARNING)
+ch.setLevel(logging.INFO)
 
 # create formatter
-formatter = logging.Formatter(
+fh_formatter = logging.Formatter(
     '%(asctime)s UTC - %(name)s - %(levelname)s - %(message)s'
+)
+ch_formatter = logging.Formatter(
+    '%(name)s - %(levelname)s - %(message)s'
 )
 
 # add formatter
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
+fh.setFormatter(fh_formatter)
+ch.setFormatter(ch_formatter)
 
 # add the handler to the logger
 logger.addHandler(fh)
@@ -79,6 +81,7 @@ def main():
 
 
 def get_network_info():
+    logger.info("Getting network info.")
     conn = wmi.WMI()
     interfaces = conn.Win32_NetworkAdapterConfiguration()
     network_info = []
@@ -93,6 +96,7 @@ def get_network_info():
 
 
 def get_physical_disk_info():
+    logger.info("Getting physical disk info.")
     conn = wmi.WMI()
     for item in conn.Win32_PhysicalMedia():
         if item.Tag == PHYSICAL_DISK_TAG:
