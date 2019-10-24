c08398fb5b0c21b111c54e0a34913f401401fdeb
jbaltop <jbaltop@gmail.com>
2019-10-24 15:07:11 +0900

    reformat

--- my_frame.py
+++ my_frame.py
@@ -59,164 +59,373 @@ class MyFrame(wx.Frame):
         key_font = wx.Font(Style.default_font, wx.DEFAULT, wx.NORMAL, wx.BOLD)
 
         i = 0
-        blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-        sizer.Add(blank_space, pos=(i, 0), flag=wx.DOWN, border=Style.section_border)
+        blank_space = wx.StaticLine(
+            panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+        )
+        sizer.Add(
+            blank_space, pos=(i, 0), flag=wx.DOWN, border=Style.section_border
+        )
         i += 1
 
-        blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-        sizer.Add(blank_space, pos=(i, 0), flag=wx.RIGHT, border=Style.section_border)
+        blank_space = wx.StaticLine(
+            panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+        )
+        sizer.Add(
+            blank_space, pos=(i, 0), flag=wx.RIGHT, border=Style.section_border
+        )
 
         line = wx.StaticText(panel, label="Network")
         line.SetFont(h1_font)
         sizer.Add(line, pos=(i, 1), span=(3, 1))
 
-        blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-        sizer.Add(blank_space, pos=(i, 2), flag=wx.RIGHT, border=Style.h1_border)
+        blank_space = wx.StaticLine(
+            panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+        )
+        sizer.Add(
+            blank_space, pos=(i, 2), flag=wx.RIGHT, border=Style.h1_border
+        )
 
         for network in self.system_info["network"]:
             line = wx.StaticText(panel, label="Name")
             line.SetFont(key_font)
-            sizer.Add(line, pos=(i, 3), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            sizer.Add(
+                line,
+                pos=(i, 3),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border
+            )
 
             line = wx.StaticText(panel, label=network["name"])
-            sizer.Add(line, pos=(i, 5), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            sizer.Add(
+                line,
+                pos=(i, 5),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 6), flag=wx.RIGHT, border=Style.section_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space,
+                pos=(i, 6),
+                flag=wx.RIGHT,
+                border=Style.section_border,
+            )
             i += 1
 
             line = wx.StaticText(panel, label="IP")
             line.SetFont(key_font)
-            sizer.Add(line, pos=(i, 3), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            sizer.Add(
+                line,
+                pos=(i, 3),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border
+            )
 
             line = wx.StaticText(panel, label=network["ip"])
-            sizer.Add(line, pos=(i, 5), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            sizer.Add(
+                line,
+                pos=(i, 5),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 6), flag=wx.RIGHT, border=Style.section_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space,
+                pos=(i, 6),
+                flag=wx.RIGHT,
+                border=Style.section_border,
+            )
             i += 1
 
             line = wx.StaticText(panel, label="MAC")
             line.SetFont(key_font)
-            sizer.Add(line, pos=(i, 3), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            sizer.Add(
+                line,
+                pos=(i, 3),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border
+            )
 
             line = wx.StaticText(panel, label=network["mac"])
-            sizer.Add(line, pos=(i, 5), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            sizer.Add(
+                line,
+                pos=(i, 5),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 6), flag=wx.RIGHT, border=Style.section_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space,
+                pos=(i, 6),
+                flag=wx.RIGHT,
+                border=Style.section_border,
+            )
             i += 1
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 0), flag=wx.DOWN, border=Style.section_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space,
+                pos=(i, 0),
+                flag=wx.DOWN,
+                border=Style.section_border,
+            )
             i += 1
 
         hline = wx.StaticLine(panel, 0, size=(20, 1), style=wx.LI_HORIZONTAL)
-        sizer.Add(hline, pos=(i, 0), span=(1, 7), flag=wx.ALIGN_CENTER | wx.EXPAND)
+        sizer.Add(
+            hline, pos=(i, 0), span=(1, 7), flag=wx.ALIGN_CENTER | wx.EXPAND
+        )
         i += 1
 
-        blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-        sizer.Add(blank_space, pos=(i, 0), flag=wx.DOWN, border=Style.section_border)
+        blank_space = wx.StaticLine(
+            panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+        )
+        sizer.Add(
+            blank_space, pos=(i, 0), flag=wx.DOWN, border=Style.section_border
+        )
         i += 1
 
-        blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-        sizer.Add(blank_space, pos=(i, 0), flag=wx.RIGHT, border=Style.section_border)
+        blank_space = wx.StaticLine(
+            panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+        )
+        sizer.Add(
+            blank_space, pos=(i, 0), flag=wx.RIGHT, border=Style.section_border
+        )
 
         line = wx.StaticText(panel, label="Boot Disk")
         line.SetFont(h1_font)
         sizer.Add(line, pos=(i, 1), span=(3, 1))
 
-        blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-        sizer.Add(blank_space, pos=(i, 2), flag=wx.RIGHT, border=Style.h1_border)
+        blank_space = wx.StaticLine(
+            panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+        )
+        sizer.Add(
+            blank_space, pos=(i, 2), flag=wx.RIGHT, border=Style.h1_border
+        )
 
         if self.system_info["os"] == "Linux":
             line = wx.StaticText(panel, label="Mount Point")
             line.SetFont(key_font)
-            sizer.Add(line, pos=(i, 3), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            sizer.Add(
+                line,
+                pos=(i, 3),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border
+            )
 
-            line = wx.StaticText(panel, label=self.system_info["boot_disk"]["mount_point"])
-            sizer.Add(line, pos=(i, 5), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            line = wx.StaticText(
+                panel, label=self.system_info["boot_disk"]["mount_point"]
+            )
+            sizer.Add(
+                line,
+                pos=(i, 5),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 6), flag=wx.RIGHT, border=Style.section_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space,
+                pos=(i, 6),
+                flag=wx.RIGHT,
+                border=Style.section_border,
+            )
             i += 1
 
             line = wx.StaticText(panel, label="Serial Number")
             line.SetFont(key_font)
-            sizer.Add(line, pos=(i, 3), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            sizer.Add(
+                line,
+                pos=(i, 3),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border
+            )
 
-            line = wx.StaticText(panel, label=str(self.system_info["boot_disk"]["serial_number"]))
-            sizer.Add(line, pos=(i, 5), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            line = wx.StaticText(
+                panel, label=str(self.system_info["boot_disk"]["serial_number"])
+            )
+            sizer.Add(
+                line,
+                pos=(i, 5),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
         else:
             line = wx.StaticText(panel, label="Device ID")
             line.SetFont(key_font)
-            sizer.Add(line, pos=(i, 3), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            sizer.Add(
+                line,
+                pos=(i, 3),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border
+            )
 
-            line = wx.StaticText(panel, label=self.system_info["boot_disk"]["device_id"])
-            sizer.Add(line, pos=(i, 5), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            line = wx.StaticText(
+                panel, label=self.system_info["boot_disk"]["device_id"]
+            )
+            sizer.Add(
+                line,
+                pos=(i, 5),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 6), flag=wx.RIGHT, border=Style.section_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space,
+                pos=(i, 6),
+                flag=wx.RIGHT,
+                border=Style.section_border,
+            )
             i += 1
 
             line = wx.StaticText(panel, label="Index")
             line.SetFont(key_font)
-            sizer.Add(line, pos=(i, 3), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            sizer.Add(
+                line,
+                pos=(i, 3),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border
+            )
 
-            line = wx.StaticText(panel, label=str(self.system_info["boot_disk"]["index"]))
-            sizer.Add(line, pos=(i, 5), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            line = wx.StaticText(
+                panel, label=str(self.system_info["boot_disk"]["index"])
+            )
+            sizer.Add(
+                line,
+                pos=(i, 5),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 6), flag=wx.RIGHT, border=Style.section_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space,
+                pos=(i, 6),
+                flag=wx.RIGHT,
+                border=Style.section_border,
+            )
             i += 1
 
             line = wx.StaticText(panel, label="Serial Number")
             line.SetFont(key_font)
-            sizer.Add(line, pos=(i, 3), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            sizer.Add(
+                line,
+                pos=(i, 3),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-            blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-            sizer.Add(blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border)
+            blank_space = wx.StaticLine(
+                panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+            )
+            sizer.Add(
+                blank_space, pos=(i, 4), flag=wx.RIGHT, border=Style.key_border
+            )
 
-            line = wx.StaticText(panel, label=self.system_info["boot_disk"]["serial_number"])
-            sizer.Add(line, pos=(i, 5), flag=wx.UP | wx.DOWN, border=Style.default_border)
+            line = wx.StaticText(
+                panel, label=self.system_info["boot_disk"]["serial_number"]
+            )
+            sizer.Add(
+                line,
+                pos=(i, 5),
+                flag=wx.UP | wx.DOWN,
+                border=Style.default_border,
+            )
 
-        blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-        sizer.Add(blank_space, pos=(i, 6), flag=wx.RIGHT, border=Style.section_border)
+        blank_space = wx.StaticLine(
+            panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+        )
+        sizer.Add(
+            blank_space, pos=(i, 6), flag=wx.RIGHT, border=Style.section_border
+        )
         i += 1
 
-        blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
-        sizer.Add(blank_space, pos=(i, 0), flag=wx.DOWN, border=Style.section_border)
+        blank_space = wx.StaticLine(
+            panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+        )
+        sizer.Add(
+            blank_space, pos=(i, 0), flag=wx.DOWN, border=Style.section_border
+        )
         i += 1
 
         # Add margin to left and right side
-        blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
+        blank_space = wx.StaticLine(
+            panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+        )
         hbox.Add(blank_space, 0, flag=wx.RIGHT, border=Style.outer_border)
 
         hbox.Add(sizer)
 
-        blank_space = wx.StaticLine(panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL)
+        blank_space = wx.StaticLine(
+            panel, 0, size=(0, 0), style=wx.LI_HORIZONTAL
+        )
         hbox.Add(blank_space, 0, flag=wx.RIGHT, border=Style.outer_border)
 
         vbox.Add(hbox, 1, flag=wx.ALIGN_CENTER)
